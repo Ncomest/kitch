3d2e747555ed75e21e853a2b5769c2de146e874b
Hurtik Igor <hurtik.igor@gmail.com>
2025-07-29 00:16:43 +0300

    update ui

--- src/components/shared/grid/catalog-kitchen/CatalogKitchen.vue
+++ src/components/shared/grid/catalog-kitchen/CatalogKitchen.vue
@@ -1,21 +1,36 @@
 <script setup>
 import ImageWrapper from "@components/shared/image-wrapper/ImageWrapper.vue";
 
-const images = [
-  { ktch: "/classic/kitchen-one.webp", id: 1 },
-  { ktch: "/classic/kitchen_2.webp", id: 2 },
-  { ktch: "/classic/kitchen_3.webp", id: 3 },
-  { ktch: "/classic/kitchen_4.webp", id: 4 },
+const kitchens = [
+  { src: "/classic/kitchen-one.webp", id: 1, discount: 10, price: 345000 },
+  { src: "/classic/kitchen_2.webp", id: 2, discount: 5, price: 315000 },
+  { src: "/classic/kitchen_3.webp", id: 3, discount: 50, price: 475000 },
+  { src: "/classic/kitchen_4.webp", id: 4, discount: null, price: 280000 },
 ];
 </script>
 
 <template>
   <section class="catalog-kitchen" id="catalog-kitchen">
     <div class="catalog-kitchen__container">
-      <h2 class="catalog-kitchen__title" >Каталог кухонь</h2>
+      <h2 class="catalog-kitchen__title">Каталог кухонь</h2>
       <div class="catalog-kitchen__grid">
-        <div class="catalog-kitchen__grid-item" v-for="image in images">
-          <ImageWrapper :src="image.ktch" alt="name" :key="image.id" class="catalog-kitchen__grid-image"/>
+        <div class="catalog-kitchen__grid-item" v-for="item in kitchens">
+          <span
+            class="catalog-kitchen__grid-item__label"
+            v-show="item.discount"
+          >
+            -{{ item.discount }}%
+          </span>
+          <ImageWrapper
+            :src="item.src"
+            alt="name"
+            :key="item.id"
+            class="catalog-kitchen__grid-image"
+          />
+          <div class="catalog-kitchen__grid-item__price-container">
+            <p class="catalog-kitchen__grid-item__price-old" v-show="item.discount">{{ item.price.toLocaleString('de-DE') }}р</p>
+            <p class="catalog-kitchen__grid-item__price">{{ item.price.toLocaleString('de-DE') }}р</p>
+          </div>
           <button class="catalog-kitchen__grid-button">Хочу такую!</button>
         </div>
       </div>
